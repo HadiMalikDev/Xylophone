e2eedd495fada41a58281af8e2c9ec1023a460d8
MHadi-Dev <Hadimalik1027@gmail.com>
2021-02-03 10:36:06 +0500

    First upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
 
@@ -10,9 +11,37 @@ class XylophoneApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+            buildExpanded(tileColor: Colors.red,noteNumber: 1),
+            buildExpanded(tileColor: Colors.orange,noteNumber: 2),
+            buildExpanded(tileColor: Colors.yellow,noteNumber: 3),
+            buildExpanded(tileColor: Colors.green,noteNumber: 4),
+            buildExpanded(tileColor: Colors.teal,noteNumber: 5),
+            buildExpanded(tileColor: Colors.blue,noteNumber: 6),
+            buildExpanded(tileColor: Colors.purple,noteNumber: 7),
+
+            ]
+            ),  
+          ),
         ),
-      ),
-    );
+      );
+    
+  }
+
+  Expanded buildExpanded({Color tileColor,int noteNumber}) {
+    return Expanded(
+            child:Container(
+              color: tileColor,
+              child: GestureDetector(
+                onTap:()
+                {
+                  final AudioCache player=AudioCache(prefix:'sounds/');
+                  player.play('note$noteNumber.wav');
+                },
+              ),
+            )  
+          );
   }
 }
